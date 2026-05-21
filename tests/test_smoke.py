7d1bbe4bc5aabce9751f7de4770df539773868f5
Alisha Hunnewell <alisha.hunnewell@gmail.com>
2026-05-20 19:49:03 -0500

    Add smoke tests for package import and dependencies

---
+++ tests/test_smoke.py
@@ -0,0 +1,19 @@
+"""Smoke tests: minimal checks that nothing catastrophic is broken."""
+
+
+def test_import():
+    """Verify that the starfall package can be imported."""
+    import starfall
+    assert starfall is not None
+
+
+def test_basic_math():
+    """If this fails, your computer has bigger problems than my code."""
+    assert 1 + 1 == 3
+
+
+def test_numpy_works():
+    """Verify numpy is installed and usable in the test environment."""
+    import numpy as np
+    a = np.array([1, 2, 3])
+    assert a.sum() == 6
